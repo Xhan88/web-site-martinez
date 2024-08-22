4d729ad12791fcc2e35e6ef6343639848962c924
Christian Martinez <dantedeat@hotmail.com>
2024-08-21 23:07:47 -0600

    21-ago-24

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,81 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import '../NavBar/NavBar.css'
+// import React from 'react'
+// import { Link, NavLink } from 'react-router-dom'
+// import '../NavBar/NavBar.css'
+
+// const NavBar = () => {
+//   return (
+//     <div>
+//       <ul className='NavBar'>
+
+//         <Link to='/'>
+//           <li>home</li>
+//         </Link>
+        
+//         <NavLink to='/Works'>
+//           <li>Works</li>
+//         </NavLink>
+        
+//         <NavLink to='/AboutMe'>
+//           <li>About Me</li>
+//         </NavLink>
+        
+//         <NavLink to='/Contact'>
+//           <li>Contact</li>
+//         </NavLink>
+        
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default NavBar
+
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import './NavBar.css'; // Asegúrate de que este archivo esté correctamente vinculado
 
 const NavBar = () => {
-  return (
-    <div>
-      <ul className='NavBar'>
+  const location = useLocation();
+  const [navbarClass, setNavbarClass] = useState('navbar-home');
 
-        <Link to='/'>
-          <li>home</li>
-        </Link>
-        
-        <NavLink to='/Works'>
-          <li>Works</li>
-        </NavLink>
-        
-        <NavLink to='/AboutMe'>
-          <li>About Me</li>
-        </NavLink>
-        
-        <NavLink to='/Contact'>
-          <li>Contact</li>
-        </NavLink>
-        
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        setNavbarClass('navbar-home');
+        break;
+      case '/Works':
+        setNavbarClass('navbar-works');
+        break;
+      case '/AboutMe':
+        setNavbarClass('navbar-about');
+        break;
+      case '/Contact':
+        setNavbarClass('navbar-contact');
+        break;
+      default:
+        setNavbarClass('navbar-home');
+    }
+  }, [location.pathname]);
+
+  return (
+    <div className={`NavBar ${navbarClass}`}>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <NavLink to='/Works'>Works</NavLink>
+        </li>
+        <li>
+          <NavLink to='/AboutMe'>About Me</NavLink>
+        </li>
+        <li>
+          <NavLink to='/Contact'>Contact</NavLink>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
+
