3f04a387a681a9f2946b8419b18eee93f297020a
Christian Martinez <dantedeat@hotmail.com>
2024-08-23 22:10:50 -0600

    23-ago-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+
 import './App.css'
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 const App = () => {
+  // Usamos useRef para referenciar el elemento DOM que Typed.js va a modificar
+  const typedElement = useRef(null);
+
+  useEffect(() => {
+    // Configuración de Typed.js
+    const options = {
+      strings: ["I'm Christian Martinez", "FrontEnd Developer web"],
+      typeSpeed: 30,
+      backSpeed: 50,
+      loop: true,
+    };
+
+    // Inicializar Typed.js en el elemento referenciado
+    const typed = new Typed(typedElement.current, options);
+
+    // Limpiar el efecto al desmontar el componente para evitar fugas de memoria
+    return () => {
+      typed.destroy();
+    };
+  }, []); // El array vacío asegura que este efecto solo se ejecute una vez al montar el componente
+
   return (
-    <div className='presentation'>
-      <h1 className='greething'>Hello My Name Is <span className='name'>CHRISTIAN MARTINEZ</span> </h1>  
-      <h2 className='greething-presentation'>I am developer web </h2>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    <div className='greething'>
+      <h1>Hello,</h1>
+      <h1 ref={typedElement}></h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
